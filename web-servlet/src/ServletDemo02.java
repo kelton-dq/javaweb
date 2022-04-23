8c7ff69ff936a0182f2898c4b533420bf44f6d28
keltontin <KeltonTin@163.com>
2022-04-23 16:09:11 +0800

    servlet初始化补充

--- web-servlet/src/ServletDemo02.java
+++ web-servlet/src/ServletDemo02.java
@@ -1,5 +1,8 @@
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,17 +13,38 @@ import java.io.IOException;
  * FileName: ServletDemo02
  * Author: Dingq
  * Date: 2022/4/19 19:51
- * Description: servlet生命周期
+ * Description: servlet生命周期：实例化、初始化、服务、销毁
  */
 
+
+@WebServlet(urlPatterns = {"/demo021"}, initParams = {@WebInitParam(name = "name", value = "dd")})
 public class ServletDemo02 extends HttpServlet {
     public ServletDemo02(){
         System.out.println("正在实例化……");
     }
 
+/*
+init方法有两个：
+    public void init(ServletConfig config) throws ServletException {
+        this.config = config;
+        this.init();
+    }
+
+    public void init() throws ServletException {
+    }
+可以重写init方法加入一些准备工作
+
+*/
+
     @Override
     public void init() throws ServletException {
         System.out.println("正在初始化……");
+        //获取初始化设置的数据
+        ServletConfig servletConfig = getServletConfig();
+        String hello = servletConfig.getInitParameter("hello");
+        System.out.println("hello = " + hello);
+        String name = servletConfig.getInitParameter("name");
+        System.out.println("name = " + name);
     }
 
     @Override
