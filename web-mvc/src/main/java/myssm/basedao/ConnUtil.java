58ec59386170ddced54f0277e94545041909eb11
keltontin <KeltonTin@163.com>
2022-04-24 20:05:42 +0800

    //

--- web-mvc/src/main/java/myssm/basedao/ConnUtil.java
+++ web-mvc/src/main/java/myssm/basedao/ConnUtil.java
@@ -55,7 +55,8 @@ public class ConnUtil {
         }
         if (!conn.isClosed()){
             conn.close();
-            threadLocal.set(null);
+//            threadLocal.set(null);
+            threadLocal.remove();
         }
     }
 }
